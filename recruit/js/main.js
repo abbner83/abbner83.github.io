6e75761c559e94bf741413356a9d909d1785d0c3
abbner <b01901146@ntu.edu.tw>
2016-05-23 09:58:19 +0800

    rwd

--- recruit/js/main.js
+++ recruit/js/main.js
@@ -1,4 +1,4 @@
-var ypos, ytot, per, setdark1, setdark2;
+var xtot, ytot, xpos, ypos, per, setdark1, setdark2, l_top, l_left;
 var s=1;
 
 $(window).load(function(){
@@ -6,15 +6,36 @@ $(window).load(function(){
 });
 
 function darken(){
+//    xtot = $(window).width();
     ytot = $(window).height();
-    ypos = pageYOffset;
-    per = Math.min(ypos/ytot,1);
-    setdark1 = "rgba(60, 40, 40," + String(per*0.8) + ")";
-    setdark2 = "rgba(60, 40, 40," + String(per*0.4+0.6) + ")";
-        $("#index1").css('background-color', String(setdark1)) ;
-        $("#index2").css('background-color', String(setdark2));
+//    xpos = (xtot*0.98 - $("#logo").width())/2;
+//    ypos = (ytot*0.96 - $("#logo").width())/2;
+    per = Math.min(pageYOffset/ytot,1);
+
+    setdark1 = "rgba(60, 40, 40," + (Math.max(per-0.8,0)*4) + ")";
+    setdark2 = "rgba(60, 40, 40," + Math.sqrt((per*0.96+0.04)) + ")";
+//    if(per<0.8){
+//        l_left = (xpos*(0.8-per) + pageXOffset*0.01) + "px";
+//        l_top = (ypos*(0.8-per) + pageYOffset*0.02) + "px";
+//    }
+//    else{
+//        l_left="1%";
+//        l_top="2%";
+//    }
     
-    setTimeout("darken()", 100);
+    $("#index1").css('background-color', setdark1) ;
+    $("#index2").css('background-color', setdark2);
+//    $("#logo").css('top', l_top);
+//    $("#logo").css('left', l_left);
+    
+    if(per>0.88){
+        $("#logo").show();
+    }
+    else{
+        $("#logo").hide();
+    }
+    
+    setTimeout("darken()", 30);
 }
 
 function fx1(){
